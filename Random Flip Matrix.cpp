19eb33b811a305c128dabf2ca7aa037dd1f22015
Komal_Goel <126601911+komal-Goel-04@users.noreply.github.com>
2025-01-22 14:20:05 +0530

    Add files via upload

---
+++ Random Flip Matrix.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+    int m, n;
+    set<pair<int, int>> s;
+
+public:
+    Solution(int m, int n)
+    {
+        this->m = m;
+        this->n = n;
+        s.clear();
+    }
+
+    vector<int> flip()
+    {
+        int r = rand() % m;
+        int c = rand() % n;
+
+        while (s.count({r, c}) > 0)
+        {
+            r = rand() % m;
+            c = rand() % n;
+        }
+
+        s.insert({r, c});
+        return {r, c};
+    }
+
+    void reset()
+    {
+        s.clear();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(m, n);
+ * vector<int> param_1 = obj->flip();
+ * obj->reset();
+ */
